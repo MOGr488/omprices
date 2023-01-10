51dcc593f58f359f6b71a51f35997aff3a3867c8
MOGr488 <mogr488@gmail.com>
2023-01-10 19:40:24 +0400

    ver 0.14

--- resources/views/searchResult.blade.php
+++ resources/views/searchResult.blade.php
@@ -12,32 +12,9 @@
 <body>
     <main>
     <h1>Search Result</h1>
-    {{-- {{ dump($products) }} --}}
+    {{ dump($products) }}
 
-    @unless (count($products) == 0)
-    <ol>
-        @foreach ($products as $product)
-        
-           <li><h3>{{ $product['productName'] }}</h3></li>
-                <ul>
-                     @foreach($product->prices as $price)
-                      
-                    {{-- <li>
-                                <strong>Store: </strong> {{ $price->store->storeName }} / 
-                                 <strong>Price: </strong> {{ $price->productPrice }} 
-                                 / <strong>wilayah: </strong> {{ $price->store->wilayah->wilayah }} -> <strong>Region: </strong> {{ $price->store->wilayah->region->regionName }} 
-                             </li> --}}
-                            @endforeach 
-                </ul>  
-        @endforeach
-
-        
-    </ol> 
-        @else
-        <h2>No Products Found</h2>
-    @endunless
-
-    {{-- <section class="py-5 text-center container">
+    <section class="py-5 text-center container">
        
     
       <div class="album py-5 bg-light">
@@ -52,16 +29,24 @@
                   <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
       
                   <div class="card-body">
-                    <h5 class="card-title">{{ $product->productName }}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <h5 class="card-title">{{ $product['productName'] }}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">{{ $product['storeName'] }}</h6>
 
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <ul class="list-group list-group-flush">
+                      
+                      <li class="list-group-item">Price : <strong>{{ $product['productPrice'] }}</strong></li>
+                     
+                    </ul>
+                    
                     <div class="d-flex justify-content-between align-items-center">
                       <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                        
+                        <a href="{{ $product['storeLocation'] }}" target="_blank" class="btn btn-sm btn-outline-secondary">Location</a>
+                        <a href="{{ $product['website'] }}" target="_blank" class="btn btn-sm btn-outline-secondary {{ empty($product['website']) ? 'disabled' : '' }}"  >Website</a>
+          
+
                       </div>
-                      <small class="text-muted">9 mins</small>
+                      <small class="text-muted">{{ $product['wilayah'] }}</small>
                     </div>
                   </div>
                 </div>
@@ -73,7 +58,7 @@
           </div>
         </div>
       </div>
-     --}}
+    
 
 </main>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
